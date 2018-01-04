40279001cc80da3fa3851929bd58137ceae1ec9a
Eric Hodel <drbrain@segment7.net>
2018-01-03 20:36:54 -0800

    Implement cool and heat setpoint handling

--- ArbitraryThermostat.indigoPlugin/Contents/Server Plugin/plugin.py
+++ ArbitraryThermostat.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -14,5 +14,30 @@ class Plugin(indigo.PluginBase):
             self.devices[device.id] = device
 
     def deviceStopComm(self, device):
-        if device.id in self.diveces:
+        if device.id in self.devices:
             self.devices.pop(device.id)
+
+    def actionControlThermostat(self, action, device):
+        self.debugLog("action %s value %r device %d" % (
+            action.thermostatAction,
+            action.actionValue,
+            device.id))
+
+        setpointCool = device.coolSetpoint
+        setpointHeat = device.heatSetpoint
+
+        if indigo.kThermostatAction.DecreaseCoolSetpoint == action.thermostatAction:
+            setpointCool -= action.actionValue
+        elif indigo.kThermostatAction.DecreaseHeatSetpoint == action.thermostatAction:
+            setpointHeat -= action.actionValue
+        elif indigo.kThermostatAction.IncreaseCoolSetpoint == action.thermostatAction:
+            setpointCool += action.actionValue
+        elif indigo.kThermostatAction.IncreaseHeatSetpoint == action.thermostatAction:
+            setpointHeat += action.actionValue
+        elif indigo.kThermostatAction.SetCoolSetpoint == action.thermostatAction:
+            setpointCool = action.actionValue
+        elif indigo.kThermostatAction.SetHeatSetpoint == action.thermostatAction:
+            setpointHeat = action.actionValue
+
+        device.updateStateOnServer("setpointCool", setpointCool)
+        device.updateStateOnServer("setpointHeat", setpointHeat)
